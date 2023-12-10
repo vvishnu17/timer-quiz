eb5b3d45a47a0b73b50e264320e6c2405b57e204
vvishnu17 <vishnuramadass17@gmail.com>
2023-12-10 13:07:21 +0100

    Fetch error handler

--- src/App.js
+++ src/App.js
@@ -6,10 +6,18 @@ import UserDetails from './Components/UserDetails';
 function App() {
   const [quizDetails,setQuizDetails] = useState([]);
   const sendRequest = useCallback(async() => {
+    try{
     const response = await fetch('https://database-request-default-rtdb.europe-west1.firebasedatabase.app/quiztimes.json')
+    if(response.status !== 200)
+    {
+      throw new Error(response.statusText)
+    }
     const data = await response.json();
     for(let key in data){
       setQuizDetails([...data[key]])
+    }}
+    catch(error){
+      console.log("Error : ",error.message);
     }
   },[])
   useEffect(()=>{
