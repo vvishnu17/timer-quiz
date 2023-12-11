9903e55cfd1b3ca5e758b13f85195c00fad2d0f2
vvishnu17 <vishnuramadass17@gmail.com>
2023-12-11 10:22:05 +0100

    add data locally

--- src/App.js
+++ src/App.js
@@ -4,6 +4,32 @@ import Challenges from './Components/Challenges';
 import UserDetails from './Components/UserDetails';
 
 function App() {
+  const localQuizDetails = [
+    {
+      targetTime: 1,
+      title: 'Easy',
+    },
+    {
+      targetTime: 5,
+      title: 'Not Easy',
+    },
+    {
+      targetTime: 10,
+      title: 'Getting Tough',
+    },
+    {
+      targetTime: 15,
+      title: 'Pros Only',
+    },
+    {
+      targetTime: 20,
+      title: 'Tougher',
+    },
+    {
+      targetTime: 25,
+      title: 'Toughest',
+    },
+  ]
   const [quizDetails,setQuizDetails] = useState([]);
   const sendRequest = useCallback(async() => {
     try{
